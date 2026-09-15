b7d149165b5d39d34936f2df21156d23a85f7dfe
Ashu Sharma <hoftrix17@gmail.com>
2026-09-15 17:21:37 +0530

    fix production google auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: stripExtensionAttrsScript }} />
       </head>
